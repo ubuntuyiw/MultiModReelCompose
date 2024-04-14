15b29d852539b98cb406da8ad1956353d2788359
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2024-04-15 01:18:29 +0300

    init

---
+++ app/src/main/java/com/ibrahimkurt/multimodreelcompose/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ibrahimkurt.multimodreelcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ibrahimkurt.multimodreelcompose.ui.theme.MultiModReelComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
