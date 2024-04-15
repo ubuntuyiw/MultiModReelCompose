ab5750b12e35bc63e1313dd694ff13e0606a63da
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2024-04-15 06:43:44 +0300

    Modernize core component module and enhance settings integration
    - Transition core component module to custom Ibrahimkurt Android and UI setup plugins for improved architectural consistency.
    - Remove outdated SDK settings and compile options, aligning the module with modern development practices.
    - Introduce the core component module into project settings, ensuring its integration within the overall project infrastructure.

---
+++ core/component/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.ibrahimkurt.android.library)
+    alias(libs.plugins.ibrahimkurt.uiSetup)
+}
+
+android {
+    namespace = "com.ibrahimkurt.core.component"
+
+}
+
+dependencies {
+
+}
