65056f1e6a2cc84346122ced02c51cf7c52df36e
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2024-04-15 03:18:12 +0300

    - Bump source and target compatibility to Java 17. - Update Kotlin jvmTarget to version 17. - Upgrade Kotlin compiler extension version to 1.5.11. - Remove unit and instrumented test dependencies. - Remove Android test implementations and test manifests. - Introduce various new libraries, versions, and plugins. - Update versions in libs.versions.toml including Kotlin, Hilt, Compose, Retrofit, and others. - Add multiple new plugins for Kotlin, Hilt, KSP, Serialization, and more.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,17 +30,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
@@ -59,11 +59,5 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
