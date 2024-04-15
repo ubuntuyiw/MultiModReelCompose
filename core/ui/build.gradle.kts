6e10c18242548b6ee5e2a79bbac71f695a0ade2c
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2024-04-15 04:54:43 +0300

    Refactor core UI module build script and update settings
    - Replace standard Android library and Kotlin Android plugins with custom Ibrahimkurt plugins.
    - Introduce the Ibrahimkurt UI setup plugin for the UI module.
    - Streamline the Android block by removing specific SDK and build settings.
    - Simplify dependencies by removing outdated or unnecessary libraries and test setups.
    - Register the core UI module in the settings.gradle.kts to include it in the project build.

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.ibrahimkurt.android.library)
+    alias(libs.plugins.ibrahimkurt.uiSetup)
+}
+
+android {
+    namespace = "com.ibrahimkurt.core.ui"
+}
+
+dependencies {
+
+}
