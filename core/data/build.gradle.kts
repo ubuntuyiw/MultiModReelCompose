a613d20f9b49d2a17959d2d21c715120cd678173
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2024-04-15 04:57:55 +0300

    Simplify core data module configuration and update project settings
    - Switch to custom Ibrahimkurt plugins for Android library, Hilt, and Retrofit Serialization in the data module.
    - Remove compileSdk, defaultConfig, and buildTypes settings to streamline the module configuration.
    - Eliminate outdated compile and kotlin options, focusing on modernizing the build setup.
    - Clear out unnecessary dependencies including core, appcompat, and material libraries.
    - Add the core data module to the project settings to ensure it's included in the overall build process.

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.ibrahimkurt.android.library)
+    alias(libs.plugins.ibrahimkurt.android.hilt)
+    alias(libs.plugins.ibrahimkurt.retrofitSerialization)
+}
+
+android {
+    namespace = "com.ibrahimkurt.core.data"
+}
+
+dependencies {
+
+}
