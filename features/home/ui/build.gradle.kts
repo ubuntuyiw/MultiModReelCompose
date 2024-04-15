ba633c24163a38f59651ece2bf0384735d548ea8
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2024-04-15 05:04:24 +0300

    Refactor home UI module and integrate into project settings
    - Replace standard Android library and Kotlin Android plugins with custom Ibrahimkurt plugins for the home UI module.
    - Implement the Ibrahimkurt UI setup plugin to optimize UI component configuration.
    - Remove legacy SDK and build configurations such as compileSdk, defaultConfig, and buildTypes to modernize the setup.
    - Streamline dependencies by removing outdated libraries and test frameworks.
    - Add the home UI module to the project settings to ensure it's included in the overall build and dependency resolution.

---
+++ features/home/ui/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.ibrahimkurt.android.library)
+    alias(libs.plugins.ibrahimkurt.uiSetup)
+}
+
+android {
+    namespace = "com.ibrahimkurt.features.home.ui"
+}
+
+dependencies {
+
+}
