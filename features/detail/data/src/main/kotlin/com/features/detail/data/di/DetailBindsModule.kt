f535a82422ed9edf3a6d641361bfe60305196afd
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2024-04-22 02:50:42 +0300

    Increase encapsulation of detail data module classes
    - Changed visibility of `DetailBindsModule` and `DetailRepositoryImpl` to `internal` to restrict access within the module, enhancing encapsulation and limiting exposure of implementation details.

--- features/detail/data/src/main/kotlin/com/features/detail/data/di/DetailBindsModule.kt
+++ features/detail/data/src/main/kotlin/com/features/detail/data/di/DetailBindsModule.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
-abstract class DetailBindsModule {
+internal abstract class DetailBindsModule {
 
     @Binds
     @ViewModelScoped
